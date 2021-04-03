8f3ca448caacdc708b552f5c787c88ddc91164f9
Brian Mann <mannb986@gmail.com>
2021-04-03 10:36:49 +0200

    code completed and tested successfully

--- wilderness_escape.py
+++ wilderness_escape.py
@@ -11,7 +11,25 @@ class TreeNode():
         self.choices = []
 
     def add_child(self, node):
-        self.choices = node
+        self.choices.append(node)
+
+    def traverse(self):
+        story_node = self
+        print(story_node.story_piece)
+
+        while len(story_node.choices) > 0:
+            choice = input("Enter 1 or 2 to continue the story: ")
+            if choice not in ['1', '2']:
+                print("Invalid entry")
+            else:
+                chosen_index = int(choice)
+                chosen_index -= 1
+                chosen_child = story_node.choices[chosen_index]
+                print(chosen_child.story_piece)
+                story_node = chosen_child
+
+        return
+
     
 
 ######
@@ -27,6 +45,7 @@ Do you:
 2 ) Run to the left...
 """)
 
+#Creating the different options for the story to take
 choice_a = TreeNode("""
 The bear is startled and runs away.
 Do you:
@@ -42,16 +61,49 @@ Do you:
 2 ) Explain that the bear scared you.
 """)
 
+choice_a_1 = TreeNode("""
+The bear returns and tells you it's been a rough week. After making peace with
+a talking bear, he shows you the way out of the forest.
+ 
+YOU HAVE ESCAPED THE WILDERNESS.
+""")
+
+choice_a_2 = TreeNode("""
+The bear returns and tells you that bullying is not okay before leaving you alone
+in the wilderness.
+ 
+YOU REMAIN LOST.
+""")
+
+choice_b_1 = TreeNode("""
+The bear is unamused. After smelling the flowers, it turns around and leaves you alone.
+ 
+YOU REMAIN LOST.
+""")
+
+choice_b_2 = TreeNode("""
+The bear understands and apologizes for startling you. Your new friend shows you a 
+path leading out of the forest.
+ 
+YOU HAVE ESCAPED THE WILDERNESS.
+""")
+
 story_root.add_child(choice_a)
 story_root.add_child(choice_b)
 
+choice_a.add_child(choice_a_1)
+choice_a.add_child(choice_a_2)
+
+choice_b.add_child(choice_b_1)
+choice_b.add_child(choice_b_2)
 
 ######
 # TESTING AREA
 ######
 
-user_choice = input("What is your name? ")
-print(user_choice)
+story_root.traverse()
 
+# user_choice = input("What is your name? ")
+# print(user_choice)
 
 # print(story_root.story_piece)
