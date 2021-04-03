f8f30c3ea9f0c6a795054a8353cf960d5f1b12e8
Brian Mann <mannb986@gmail.com>
2021-04-03 09:51:57 +0200

    tree class created and story root defined

--- wilderness_escape.py
+++ wilderness_escape.py
@@ -1,11 +1,29 @@
+print("Once upon a time…")
+
 ######
 # TREENODE CLASS
 ######
+
+class TreeNode():
+  def __init__(self, story_piece):
+        self.story_piece = story_piece
+        self.choices = []
 
 ######
 # VARIABLES FOR TREE
 ######
 
+story_root = TreeNode("""
+You are in a forest clearing. There is a path to the left.
+A bear emerges from the trees and roars!
+Do you: 
+1 ) Roar back!
+2 ) Run to the left...
+""")
+
+
 ######
 # TESTING AREA
 ######
+
+print(story_root.story_piece)
