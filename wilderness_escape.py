5a97d29982ddc2b45e0d5751308db273ccec31c5
Brian Mann <mannb986@gmail.com>
2021-04-03 10:00:02 +0200

    child nodes added to give options to start of the story

--- wilderness_escape.py
+++ wilderness_escape.py
@@ -5,14 +5,20 @@ print("Once upon a time…")
 ######
 
 class TreeNode():
-  def __init__(self, story_piece):
+
+    def __init__(self, story_piece):
         self.story_piece = story_piece
         self.choices = []
+
+    def add_child(self, node):
+        self.choices = node
+    
 
 ######
 # VARIABLES FOR TREE
 ######
 
+#Creating root i.e. start of story
 story_root = TreeNode("""
 You are in a forest clearing. There is a path to the left.
 A bear emerges from the trees and roars!
@@ -21,9 +27,31 @@ Do you:
 2 ) Run to the left...
 """)
 
+choice_a = TreeNode("""
+The bear is startled and runs away.
+Do you:
+1 ) Shout 'Sorry bear!'
+2 ) Yell 'Hooray!'
+""")
+
+choice_b = TreeNode("""
+You come across a clearing full of flowers. 
+The bear follows you and asks 'what gives?'
+Do you:
+1 ) Gasp 'A talking bear!'
+2 ) Explain that the bear scared you.
+""")
+
+story_root.add_child(choice_a)
+story_root.add_child(choice_b)
+
 
 ######
 # TESTING AREA
 ######
 
-print(story_root.story_piece)
+user_choice = input("What is your name? ")
+print(user_choice)
+
+
+# print(story_root.story_piece)
